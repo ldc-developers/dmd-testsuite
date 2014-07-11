125cec57c5efeed51ccd3b9ed2dc84bfca5db81b
David Nadlinger <code@klickverbot.at>
2014-07-11 17:20:53 +0200

    Combine inline IR tests.
    This also removes the last use of the (renamed)
    llvm_inline_ir pragma.

--- runnable/ldc_llvm_inline_ir.d
+++ runnable/ldc_llvm_inline_ir.d
@@ -39,6 +39,13 @@ alias inlineIR!(`
     ret i64 %ret`,
     long, double, double) lt; 
 
+alias __vector(float[4]) float4;
+alias inlineIR!(`
+    %r = fadd <4 x float> %0, %1
+    ret <4 x float> %r`, float4, float4, float4) foo;
+
+alias inlineIR!(`store i32 %1, i32* %0`, void, int*, int) bar;
+
 void main()
 {
     assert(factorial(6) == 720);
