da854e03f4b75af110a6aba53c10b881a812bd8d
David Nadlinger <code@klickverbot.at>
2015-07-13 20:43:45 +0200

    compilable/test7030: Adapt for difference in handling leading underscores
    See ldc-developers/ldc#114.

--- compilable/test7030.d
+++ compilable/test7030.d
@@ -8,7 +8,12 @@ extern(C++)
     }
 }
 
-version (OSX)
+// DMD adds the extra underscore that is required for platform ABI compliance
+// to the mangleof string, see https://issues.dlang.org/show_bug.cgi?id=8207 and
+// LDC GitHub issue #114.
+version (DigitalMars) version (OSX) version = DMD_OSX;
+
+version (DMD_OSX)
 {
     static assert(S.foo.mangleof == "__ZNK1S3fooEi");
     static assert(S.bar.mangleof == "__ZN1S3barEi");
