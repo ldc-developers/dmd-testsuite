49d774402b627583ae6172d94d332777f2e11d3f
Martin <noone@nowhere.com>
2015-08-07 23:47:32 +0200

    Improve Windows-friendliness for MSVC builds.
    The only essential dependencies are GNU make for Windows and bash
    with some tools (diff, grep...). bash and GNU tools are provided
    by git for Windows distributions.
    
    This commit doesn't overwrite the LIB environment variable (which
    is crucial for the MS linker) and assumes cl.exe to be in a directory
    in the PATH environment variable rather than a hardcoded path to
    VS 2010.
    
    A binary distribution of GNU make for Windows (from 2006) is available
    at http://gnuwin32.sourceforge.net/packages/make.htm - you'll need the
    binaries and dependencies ZIPs.
    Some scripts require GNU diff to support `--strip-trailing-cr`, which
    wasn't the case for the ancient diff.exe (v2.7) provided by my git
    distribution. A newer version (2.8.7) can be downloaded from
    http://gnuwin32.sourceforge.net/packages/diffutils.htm.

--- d_do_test.d
+++ d_do_test.d
@@ -427,6 +427,8 @@ bool compareOutput(string output, string refoutput)
     }
 }
 
+version(LDC) version(CRuntime_Microsoft) version = LDC_MSVC;
+
 int main(string[] args)
 {
     if (args.length != 4)
@@ -479,8 +481,16 @@ int main(string[] args)
     {
         switch (envData.os)
         {
+          version(LDC_MSVC)
+          {
+            case "win32": envData.ccompiler = "cl.exe"; break;
+            case "win64": envData.ccompiler = "cl.exe"; break;
+          }
+          else
+          {
             case "win32": envData.ccompiler = "dmc"; break;
             case "win64": envData.ccompiler = `\"Program Files (x86)"\"Microsoft Visual Studio 10.0"\VC\bin\amd64\cl.exe`; break;
+          }
             default:      envData.ccompiler = "g++"; break;
         }
     }
