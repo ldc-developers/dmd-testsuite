4238acad391cc9642ecb6e45280389865b9feb7e
Kai Nacke <kai@redstar.de>
2016-03-06 22:16:07 +0100

    Add test case for issue #1292.

---
+++ compilable/ldc_github_1292.d
@@ -0,0 +1,16 @@
+void main()
+{
+    fun();
+}
+
+void fun()
+{
+    double x0 = 0,
+           x1 = 1;
+
+    asm nothrow @nogc
+    {
+        movlpd qword ptr x0, XMM0;
+        movhpd qword ptr x1, XMM0;
+    }
+}
