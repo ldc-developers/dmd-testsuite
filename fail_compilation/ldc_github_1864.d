0889fc7ce6f2148e1278e6f04e28ca50399d431b
Martin <noone@nowhere.com>
2017-08-04 02:03:10 +0200

    Adapt to backticks in error messages

--- fail_compilation/ldc_github_1864.d
+++ fail_compilation/ldc_github_1864.d
@@ -1,7 +1,7 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/ldc_github_1864.d(16): Error: function ldc_github_1864.templateDecl!(Aggr!(var)).templateDecl cannot access frame of function ldc_github_1864.foo
+fail_compilation/ldc_github_1864.d(16): Error: function `ldc_github_1864.templateDecl!(Aggr!(var)).templateDecl` cannot access frame of function `ldc_github_1864.foo`
 ---
 */
 
