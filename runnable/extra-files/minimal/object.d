4f08cfc4e17bc0b2d5e56eb35c0a1f5de26674ee
Martin <noone@nowhere.com>
2018-04-01 23:25:07 +0200

    Fix signature of _d_run_main() in runnable/extra-files/minimal/object.d

--- runnable/extra-files/minimal/object.d
+++ runnable/extra-files/minimal/object.d
@@ -1,7 +1,8 @@
 module object;
 
 private alias extern(C) int function(char[][] args) MainFunc;
-private extern (C) int _d_run_main(int argc, char** argv, MainFunc mainFunc)
+private extern (C) int _d_run_main(int argc, char** argv, void* _mainFunc)
 {
+    auto mainFunc = cast(MainFunc) _mainFunc;
     return mainFunc(null);
 }
