a50a6107ffe3440da0d4d79f8834acc5c914ada8
Martin <noone@nowhere.com>
2018-05-16 23:22:39 +0200

    Slightly adapt runnable/externmangle.d wrt. uint64_t etc.

--- runnable/externmangle.d
+++ runnable/externmangle.d
@@ -1,7 +1,5 @@
 // EXTRA_CPP_SOURCES: externmangle.cpp
 
-version (OSX) version (X86) version = OSX_X86;
-
 extern(C++):
 
 struct Foo(X)
@@ -124,11 +122,16 @@ interface Module
     public static int dim(Array!Module*);
 };
 
-import core.stdc.config;
-cpp_uint64_t testlongmangle(int a, uint b, cpp_int64_t c, cpp_uint64_t d);
+import core.stdc.config; // for cpp_(u)long(long)
+import core.stdc.stdint : int64_t, uint64_t;
+uint64_t testlongmangle(int a, uint b, int64_t c, uint64_t d);
 cpp_ulong testCppLongMangle(cpp_long a, cpp_ulong b);
 cpp_ulonglong testCppLongLongMangle(cpp_longlong a, cpp_ulonglong b);
-size_t testCppSizeTMangle(ptrdiff_t a, size_t b);
+version (OSX) version (X86) version = OSX_X86;
+version (OSX_X86)
+    size_t testCppSizeTMangle(cpp_ptrdiff_t a, cpp_size_t b);
+else
+    size_t testCppSizeTMangle(ptrdiff_t a, size_t b);
 
 __gshared extern int[2][2][2] test31;
 __gshared extern int* test32;
@@ -285,10 +288,9 @@ void main()
     arr2.dim = 20;
     assert(Module.dim(&arr2) == 20);
 
-    assert(testlongmangle(1, 2, cpp_int64_t(3), cpp_uint64_t(4)) == 10);
-    assert(testCppLongMangle(cpp_long(1), cpp_ulong(2)) == 3);
-    assert(testCppLongLongMangle(cpp_longlong(3), cpp_ulonglong(4)) == 7);
-    version (OSX_X86) { /* C++ size_t is `unsigned long` */ } else
+    assert(testlongmangle(1, 2, 3, 4) == 10);
+    assert(testCppLongMangle(1, 2) == 3);
+    assert(testCppLongLongMangle(3, 4) == 7);
     assert(testCppSizeTMangle(3, 4) == 7);
     assert(test31 == [[[1, 1], [1, 1]], [[1, 1], [1, 1]]]);
     assert(test32 == null);
