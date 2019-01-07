755840d312d2d81b71259b53131a3bb3dcd990f9
Martin Kinkelin <noone@nowhere.com>
2019-01-07 16:03:14 +0100

    Enable fail_compilation/b17918.d for LDC

--- fail_compilation/b17918.d
+++ fail_compilation/b17918.d
@@ -1,5 +1,3 @@
-// LDC emits an additional error ('failed to infer return type for vtbl initializer')
-// DISABLED: LDC
 /* TEST_OUTPUT:
 ---
 fail_compilation/imports/b17918a.d(7): Error: undefined identifier `_listMap`
