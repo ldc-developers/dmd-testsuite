d978d77ae343c87af567b207653801cfb12bb12d
Temtaime <temtaime@gmail.com>
2019-03-20 20:34:11 +0300

    Fix test for #3034
    Seems be be a fix.
    @kinke

--- runnable/test13774.sh
+++ runnable/test13774.sh
@@ -2,7 +2,13 @@
 
 set -e
 
-$DMD -m${MODEL} -I${EXTRA_FILES} -lib -of${OUTPUT_BASE}a${LIBEXT} ${EXTRA_FILES}${SEP}lib13774a.d
-$DMD -m${MODEL} -I${EXTRA_FILES} -lib -of${OUTPUT_BASE}b${LIBEXT} ${EXTRA_FILES}${SEP}lib13774b.d ${OUTPUT_BASE}a${LIBEXT}
+EXTRA_ARGS=()
+
+if [[ "$OS" == "win"* ]]; then
+	EXTRA_ARGS+=(-ar=lib.exe)
+fi
+
+$DMD -m${MODEL} -I${EXTRA_FILES} -lib "${EXTRA_ARGS[@]}" -of${OUTPUT_BASE}a${LIBEXT} ${EXTRA_FILES}${SEP}lib13774a.d
+$DMD -m${MODEL} -I${EXTRA_FILES} -lib "${EXTRA_ARGS[@]}" -of${OUTPUT_BASE}b${LIBEXT} ${EXTRA_FILES}${SEP}lib13774b.d ${OUTPUT_BASE}a${LIBEXT}
 
 rm ${OUTPUT_BASE}{a${LIBEXT},b${LIBEXT}}
