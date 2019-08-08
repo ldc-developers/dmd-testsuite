e69d5b2e018deb72ee5d5a3d003a5069ce1156f0
Martin Kinkelin <noone@nowhere.com>
2019-08-08 22:07:12 +0200

    [LDC] fail_compilation/test20096.d: Adapt expected output

--- fail_compilation/test20096.d
+++ fail_compilation/test20096.d
@@ -1,7 +1,7 @@
 /*
 TEST_OUTPUT:
 ---
-fail_compilation/test20096.d(19): Error: cannot `goto` into `try` block
+fail_compilation/test20096.d(15): Error: `goto` into `try`/`finally` scope is not allowed
 ---
 */
 // https://issues.dlang.org/show_bug.cgi?id=20096
