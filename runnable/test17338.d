ae8200298e9396c83ef63ea14af77d3a4a65aff0
Martin Kinkelin <noone@nowhere.com>
2020-01-24 01:48:49 +0100

    LDC: Disable runnable/test17338.d (by far the longest running test)
    With -O, it takes 54 seconds on my Windows box; on Linux, it's a whopping
    75 secs (about 3x longer than the 2nd-longest test). With -g, it's still
    about 25 seconds.
    
    Fortunately, the test is rather uninteresting for LDC.

--- runnable/test17338.d
+++ runnable/test17338.d
@@ -1,3 +1,11 @@
+/* DISABLED: LDC
+ *
+ * This is a DMD codegen regression test which takes veeery long and severely
+ * impacts parallelizability of dmd-testsuite. As d_do_test tries to run
+ * disabled tests too, we need to disable the whole module.
+ */
+version (LDC) { /* will fail because of missing main */ } else:
+
 // PERMUTE_ARGS:
 // Generate \sum_{i=0}^{14} 2^i = 32767 template instantiations
 // (each with 3 sections) to use more than 64Ki sections in total.
