a4dc6893688c12375f80466f4dd164397b755b12
Martin Kinkelin <noone@nowhere.com>
2022-04-27 13:58:07 +0200

    LDC: Adapt expected output of fail_compilation/write_error.d some more

--- fail_compilation/write_error.d
+++ fail_compilation/write_error.d
@@ -4,7 +4,7 @@ https://issues.dlang.org/show_bug.cgi?id=23019
 ARG_SETS: -of=fail_compilation
 TEST_OUTPUT:
 ---
-Error: cannot write file 'fail_compilation': Is a directory
+Error: cannot write file 'fail_compilation': $r:(i|I)s$ a directory
 ---
 ++/
 
