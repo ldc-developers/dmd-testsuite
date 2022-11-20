9b83c02604d72c239b1e8122142c90681be76a08
Martin Kinkelin <mkinkelin@symmetryinvestments.com>
2022-11-20 03:46:54 +0100

    LDC: Minimally adapt a test to moved submodule path in LDC repo

--- runnable/paranoia.d
+++ runnable/paranoia.d
@@ -6,8 +6,8 @@ ARG_SETS: -version=Single
 ARG_SETS: -version=Double
 
 Windows targets adapted for LDC:
-ARG_SETS(windows): -version=Extended ../../../dmd/root/longdouble.d
-ARG_SETS(windows): -version=ExtendedSoft ../../../dmd/root/longdouble.d
+ARG_SETS(windows): -version=Extended ../../dmd/root/longdouble.d
+ARG_SETS(windows): -version=ExtendedSoft ../../dmd/root/longdouble.d
 
 ARG_SETS(linux osx): -version=Extended
 */
